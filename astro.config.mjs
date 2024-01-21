7fd431fe38fc856fca774794c30373965985a0d5
Debashis Roy <goodquote39@hotmail.com>
2024-01-21 09:22:21 +0600

    practice a little

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: { host: "0.0.0.0", port: 3000 },
 });
